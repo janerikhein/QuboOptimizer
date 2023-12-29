15b5fe81bbe14ff85f8493a409734b7075274552
janerikhein <janerik.hein@gmail.com>
2023-12-29 20:49:04 +0100

    added search parameters ans searchstate structs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod qubo {
+    //TODO: implement preprocessing rules for QuboInstance
     use ndarray::{Array1, Array2};
 
     pub struct QuboInstance {
@@ -7,15 +8,15 @@ mod qubo {
         // boolean solution vector
         solution: Option<Array1<bool>>,
         // objective value
-        obj_val : Option<f64>,
+        obj_val: Option<f64>,
         // objective changes when flipping variables
-        flip_values : Option<Array1<f64>>,
+        flip_values: Option<Array1<f64>>,
     }
 
     // TODO: Discuss whther should allow for generic types instead of only f64.
     impl QuboInstance {
         pub fn new(q: Array2<f64>, solution: Option<Array1<bool>>) -> QuboInstance {
-            let mut inst = QuboInstance {q, solution: None, obj_val: None, flip_values: None};
+            let mut inst = QuboInstance { q, solution: None, obj_val: None, flip_values: None };
             if solution.is_some() {
                 inst.set_solution(solution.unwrap());
             }
@@ -70,7 +71,6 @@ mod qubo {
         pub fn is_active(&self, i: u32, j: u32) {
             self.solution[i] & self.solution[j]
         }
-
     }
 }
 
@@ -92,9 +92,9 @@ mod qubo_start_heuristic {
     impl StartHeuristic {
         pub fn get_solution(&self, qubo: &QuboInstance) -> Array1<bool> {
             match self {
-                StartHeuristic::Random => {StartHeuristic::get_solution_rand(qubo)}
+                StartHeuristic::Random => { StartHeuristic::get_solution_rand(qubo) }
                 //TODO: add (parameterized) heuristics here
-                StartHeuristic::GreedyRounding(plch) => {StartHeuristic::get_solution_greedy_rounding(qubo, plch)}
+                StartHeuristic::GreedyRounding(plch) => { StartHeuristic::get_solution_greedy_rounding(qubo, plch) }
             }
         }
 
@@ -128,14 +128,83 @@ mod qubo_start_heuristic {
             // min
         }
     }
-
 }
 
 mod tabu_search {
+    use ndarray::{Array1, Array2};
+    use rand::rngs::StdRng;
+    use crate::qubo::QuboInstance;
 
+    enum SearchMode {
+        // Default search mode using standard objective
+        Default,
+        // Diversification mode using modified objective with a given frequency penalty factor
+        Diversify(f64),
+    }
+
+    struct SearchParameters {
+        // Minimum number of iterations a performed move is marked tabu
+        min_tabu_length: u32,
+        // Minimum number of iterations a performed move is marked tabu, relative to problem size
+        tabu_length_rel: f64,
+        // max random offset for tabu length (uniformly chosen), i.e the actual length of the tabu tenure is given by
+        // max(<min_tabu_length>, <tabu_length_rel> * n) + Rand(0,...,<max_tabu_length_offset>)
+        max_tabu_length_offset: u32,
+        // Maximum number of non-improving moves until default phase terminates
+        default_non_improvement_threshold: u32,
+        // Maximum number of non-improving moves until diversification phase terminates
+        diversify_non_improvement_threshold: u32,
+        // base factor for penalizing frequently activated/deactivated entries during diversification
+        freq_penalty_base: f64,
+        // scale factor by which frequency penalty gets increases after unsuccessful phase, i.e. a default phase, where
+        // no global improvement was found and the search returned again to previous local maximum
+        freq_penalty_scale: f64,
+        // seed for random number generator
+        seed: Option<u64>,
+    }
+
+    impl SearchParameters {
+        //TODO: Read default parameters from file instead of hardcoding
+        fn default_parameters() -> SearchParameters {
+            SearchParameters {
+                min_tabu_length: 5,
+                tabu_length_rel: 0.01,
+                max_tabu_length_offset: 10,
+                default_non_improvement_threshold: 50,
+                diversify_non_improvement_threshold: 5,
+                freq_penalty_base: 0.1,
+                freq_penalty_scale: 1.5,
+                seed: None,
+            }
+        }
+
+        fn set_fixed_seed(self, seed: u64) -> SearchParameters {
+            SearchParameters {
+                seed: Some(seed),
+                ..self
+            }
+        }
+    }
+
+    struct TabuSearchState {
+        // qubo instance with current solution
+        qubo: QuboInstance,
+        // search iteration
+        it: u32,
+        // last iteration with global improvement
+        last_improved: u32,
+        // tabu tenures, i.e. move x is tabu if tabu[x] >= it
+        tabu: Array1<u32>,
+        // swap frequency count
+        swap_count: Array2<u32>,
+        // current search mode of the phase
+        search_mode: SearchMode,
+        // Random number generator
+        rng: StdRng,
+        // constant search parameters
+        search_parameters: SearchParameters,
+    }
 }
 
 
-fn main() {
-
-}
+fn main() {}
