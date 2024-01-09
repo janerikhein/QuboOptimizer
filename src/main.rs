1b16c1e0f245e0f45d4f0ba4722b4c4281cdc70b
FettGoenner <s83251@bht-berlin.de>
2024-01-09 16:27:38 +0100

    greedy rounding done?

--- src/main.rs
+++ src/main.rs
@@ -24,32 +24,32 @@ mod qubo {
         // TODO: test this
         // Set a new solution to the instance
         pub fn set_solution(&mut self, solution: Array1<bool>) {
-            assert_eq!(solution.len(), self.nvars());
+            assert_eq!(solution.len(), self.nvars() as usize);
             if self.solution.is_none() {
                 // Initilize to zero vector
-                self.solution = Some(Array1::zeros(self.nvars()));
+                self.solution = Some(Array1::from_elem(self.nvars(), false));
                 // For all zero solution, flip values are given by values of Q on diagonal
                 self.flip_values = Some(self.q.diag().to_owned());
                 // objective for zero vector is zero
                 self.obj_val = Some(0.0);
             }
-            let cur_sol = self.solution.unwrap();
+            let cur_sol = self.solution.clone().unwrap();
             for (index, (&val1, &val2)) in cur_sol.iter().zip(solution.iter()).enumerate() {
                 if val1 != val2 {
-                    self.flip(index as u32);
+                    self.flip(index);
                 }
             }
         }
 
         // number of variables
-        pub fn nvars(&self) -> u32 {
+        pub fn nvars(&self) -> usize {
             // TODO: return size of Q, i.e. number of rows or columns
-            todo!()
+            0
         }
 
         // TODO: test this
-        pub fn get_matrix(&self) -> Array2 {
-            self.q
+        pub fn get_matrix(&self) -> &Array2<f64> {
+            &self.q
         }
 
         pub fn get_obj(&self) -> f64 {
@@ -57,13 +57,17 @@ mod qubo {
         }
 
         // get objective difference of applying a flip to a given xi
-        pub fn get_obj_delta_on_flip(&self, flip_index: u32) -> f64 {
+        pub fn get_obj_delta_on_flip(&self, flip_index: usize) -> f64 {
             assert!(flip_index < self.nvars());
-            self.flip_values.expect("No solution set for QuboInstance.")[flip_index]
+            match &self.flip_values {
+                Some(f) => f[flip_index],
+                None    => std::f64::MIN,
+            }
+            //self.flip_values.expect("No solution set for QuboInstance.")[flip_index]
         }
 
         // Apply a variable flip for a given xi
-        pub fn flip(&mut self, flip_index: u32) {
+        pub fn flip(&mut self, flip_index: usize) {
             // TODO: modify flip values accordingly
             // TODO: update objective value
             //
@@ -71,10 +75,12 @@ mod qubo {
             todo!()
         }
 
-        pub fn is_active(&self, i: u32, j: u32) {
-            self.solution[i] & self.solution[j]
+        pub fn is_active(&self, i: usize, j: usize) {
+            match &self.solution {
+                Some(s) => s[i] & s[j],
+                None    => false,
+            };
         }
-
     }
 }
 
@@ -113,37 +119,37 @@ mod qubo_start_heuristic {
 
         /// This is O(n^2) TODO: Test
         fn get_solution_greedy_rounding(qubo: &QuboInstance, hint: &f64) -> Array1<bool> {
-            let n = qubo.nvars();
             let q = qubo.get_matrix();
-            let mut hint_vec = Array1::from_elem((n), hint);
+            let n = q.nrows();
+            let hint_vec = Array1::from_elem(n, hint);
             //let mut diff = Array1::from_elem((n), 0.0);
             let mut obj_val = 0.0;
             // Calculate the obj_val for the entire hint vector in O(n^2)
             for i in 0..n {
                 for j in 0..n {
-                    obj_val = q[i,j]*hint*hint;
+                    obj_val = q[[i,j]]*hint*hint;
                 }
             }
             // TODO: randomize array access via random permutation
-            let mut solution = Array1::from_elem((n), false);
+            let mut solution = Array1::from_elem(n, false);
             for k in 0..n {
                 // subtract old "sum cross"
                 for i in 0..n {
-                    obj_val -= q[i,k]*hint_vec[i]*hint_vec[k];
+                    obj_val -= q[[i,k]]*hint_vec[i]*hint_vec[k];
                 }
                 for j in 0..n {
                     if j == k { continue; } // already subtracted for [k,k]
-                    obj_val -= q[k,j]*hint_vec[j]*hint_vec[k];
+                    obj_val -= q[[k,j]]*hint_vec[j]*hint_vec[k];
                 }
-                // add new "sum cross" for hint_vec[k] = 0, which is 0
+                // "sum cross" for hint_vec[k] = 0 is itself equal to 0
                 // add new "sum cross" for hint_vec[k] = 1
                 let mut obj_val_true = obj_val;
                 for i in 0..n {
-                    obj_val_true += q[i,k]*hint_vec[i]; // * 1
+                    obj_val_true += q[[i,k]]*hint_vec[i]; // * 1
                 }
                 for j in 0..n {
                     if j == k { continue; } // already added for [k,k]
-                    obj_val_true += q[k,j]*hint_vec[j]; // * 1
+                    obj_val_true += q[[k,j]]*hint_vec[j]; // * 1
                 }
                 // Set solution at [k]
                 if obj_val_true < obj_val {
@@ -161,7 +167,24 @@ mod tabu_search {
 
 }
 
-
 fn main() {
 
+}
+
+#[cfg(test)]
+mod tests {
+    use ndarray::prelude::*;
+    use crate::qubo::QuboInstance;
+    use crate::qubo_start_heuristic::StartHeuristic;
+
+    #[test]
+    fn greedy_rounding() {
+        let q = Array2::<f64>::from_diag(&arr1(&[-3.0, 2.0, -2.0]));
+        let correct_solution = array![true, false, true];
+        let n = 3;
+        //let q = Array2::<f64>::eye(n);
+        let qubo = QuboInstance::new(q, None);
+        let solution = StartHeuristic::GreedyRounding(0.5).get_solution(&qubo);
+        assert_eq!(solution, correct_solution);
+    }
 }
