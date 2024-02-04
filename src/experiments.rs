99c6dd1e77f2c30a64be0c2546f493b121281d30
FettGoenner <s83251@bht-berlin.de>
2024-02-04 13:57:15 +0100

    test_tabu_search_params started

--- src/experiments.rs
+++ src/experiments.rs
@@ -4,7 +4,7 @@ use std::io::stdin;
 use crate::qubo::*;
 use crate::preprocess;
 use crate::start_heuristics::StartHeuristic;
-use crate::tabu_search;
+use crate::tabu_search::*;
 use ndarray_stats::QuantileExt;
 
 const INST_DIR: &str = "instances/";
@@ -29,50 +29,70 @@ pub fn example() {
     let start_heuristic = StartHeuristic::GreedyFromHint(0.5);
     let start_solution = start_heuristic.get_solution(&qubo);
     // 4) Do tabu search heuristic
-    let good_solution = tabu_search::tabu_search(&qubo, &start_solution);
+    //let good_solution = tabu_search::tabu_search(&qubo, &start_solution);
+}
+
+
+pub fn test_tabu_search_params() {
+    // let params = ModelParameters {
+    //     tenure_ratio: f64,
+    //     diversification_base_factor: f64,
+    //     diversification_scaling_factor: f64,
+    //     improvement_threshold: usize,
+    //     blocking_move_number: usize,
+    //     activation_function: ActivationFunction,
+    // }
+    let instances = ["bqp50.1",];
+    // Pseudocode:
+    // let x = start_vec
+    // for i in instances {
+    //     let qubo = QuboInstance::from_file(i);
+    //     for _ in params {
+    //         let obj_vals = (...);
+    //         for _ in param_vals {
+    //             let solution = tabu_search(qubo, x)
+    //             obj_val[...] = qubo.compute_objective(x);
+    //         }
+    //         let best = obj_vals.argmin();
+    //         println!("{param}, {}", param_vals[best]);
+    //     }
+    // }
 }
 
 pub fn test_start_heuristics() {
-    fn create_vecs(qubo: &QuboInstance) -> (Vector, Vector) {
-        let n = qubo.size();
-        let mut  p = vec![0; n];  // number of positives
-        let mut  z = vec![0; n];  // number of nonzeros
-        let mut sp = vec![0.; n]; // sum of positives
-        let mut sn = vec![0.; n]; // sum of negatives
-        for i in 0..n {
-            for j in i..n {
-                // Run through row i
-                let entry_ij = qubo.get_entry_at(i, j);
-                if entry_ij > 0. {
-                    p[i] += 1;
-                    z[i] += 1;
-                    sp[i] += entry_ij;
-                }
-                else if entry_ij < 0. {
-                    z[i] += 1;
-                    sn[i] += entry_ij;
-                }
-            }
-        }
-        let mut a = Vector::from_vec(vec![0.; n]);
-        let mut b = Vector::from_vec(vec![0.; n]);
-        for i in 0..n {
-            if   z[i] != 0 { a[i] = (p[i] as f64)/(z[i] as f64); }
-            else { a[i] = 1.; }
-            if sp[i] - sn[i] != 0. { b[i] = sp[i]/(sp[i] - sn[i]); }
-            else { b[i] = 1.; }
-        }
-        (a, b)
-    }
     //TODO: greedy_multiple_steps?
-    let instances = ["p3000.1", "p3000.4", "p6000.1", "p6000.3"];
+    let instances = [
+        "bqp50.1",
+        "bqp50.2",
+        "bqp50.3",
+        "bqp100.1",
+        "bqp100.2",
+        "bqp100.3",
+        "bqp250.1",
+        "bqp250.2",
+        "bqp250.3",
+        "bqp500.1",
+        "bqp500.2",
+        "bqp500.3",
+        "bqp1000.1",
+        "bqp1000.2",
+        "bqp1000.3",
+        "bqp2500.1",
+        "bqp2500.2",
+        "bqp2500.3",
+        "p3000.1",
+        "p3000.4",
+        "p6000.1",
+        "p6000.3",
+    ];
     println!("Compare start heuristics for instances {instances:?}");
+    let mut goodness = Vector::from_vec(vec![0.; 4]);
     for i in instances {
         println!("--- Starting for {i} ---");
         let filename = INST_DIR.to_owned() + i;
         let qubo = QuboInstance::from_file(&filename);
         let n = qubo.size();
-        let (a, b) = create_vecs(&qubo);
+        let (a, b) = create_hint_vecs(&qubo);
         let heuristics = [
             StartHeuristic::Random(42),
             StartHeuristic::GreedyFromVec(Vector::from_vec(vec![0.5; n])),
@@ -90,17 +110,26 @@ pub fn test_start_heuristics() {
                 avg_time += now.elapsed();
             }
             obj_vals[k] = qubo.compute_objective(sol);
+            goodness[k] += obj_vals[k];
             avg_time /= 10;
             println!(
                 "#{k} {h:?}: {}, took {avg_time:.2?} on 10 run avg.",
-                obj_vals[k]);
+                obj_vals[k]
+            );
         }
         let best = obj_vals.argmin().unwrap();
         println!(
-            "--- Done. #Best={best}: {:?}, {} ---",
+            "--- Done. #{best} is best: {:?}, {} ---",
             heuristics[best],
-            obj_vals[best]);
+            obj_vals[best]
+        );
     }
+    let best = goodness.argmin().unwrap();
+    println!(
+        "#{best} is best overall with goodness {} vs.{}",
+        goodness[best],
+        goodness,
+    );
 }
 
 /// Another experiment
@@ -111,4 +140,59 @@ pub fn foo() {
 /// Yet another experiment
 pub fn bar() {
     todo!();
+}
+
+/// Helper function for start heuristic testing
+fn create_hint_vecs(qubo: &QuboInstance) -> (Vector, Vector) {
+    let n = qubo.size();
+    let mut negs = vec![0; n];  // number of negatives
+    let mut nzrs = vec![0; n];  // number of nonzeros
+    let mut sneg = vec![0.; n]; // sum of negatives
+    let mut spos = vec![0.; n]; // sum of positives
+    // x_i
+    for i in 0..n {
+        // Row i
+        for j in i..n {
+            let entry_ij = qubo.get_entry_at(i, j);
+            if entry_ij > 0. {
+                nzrs[i] += 1;
+                spos[i] += entry_ij;
+            }
+            else if entry_ij < 0. {
+                nzrs[i] += 1;
+                negs[i] += 1;
+                sneg[i] += entry_ij;
+            }
+        }
+        // Col i
+        for j in 0..i {
+            let entry_ji = qubo.get_entry_at(j, i);
+            if entry_ji > 0. {
+                nzrs[i] += 1;
+                spos[i] += entry_ji;
+            }
+            else if entry_ji < 0. {
+                nzrs[i] += 1;
+                negs[i] += 1;
+                sneg[i] += entry_ji;
+            }
+        }
+    }
+    let mut a = Vector::from_vec(vec![0.; n]);
+    let mut b = Vector::from_vec(vec![0.; n]);
+    for i in 0..n {
+        if   nzrs[i] != 0 {
+            a[i] = (negs[i] as f64)/(nzrs[i] as f64);
+        }
+        else {
+            a[i] = 1.;
+        }
+        if sneg[i] - spos[i] != 0. {
+            b[i] = sneg[i]/(sneg[i] - spos[i]);
+        }
+        else {
+            b[i] = 1.;
+        }
+    }
+    (a, b)
 }
